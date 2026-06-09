531f8326508d728f349a561bd923dca019aeac34
Atharva <atharvakumbhar631@gmail.com>
2026-06-09 16:53:09 +0530

    error page

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import type {Request, Response} from "express";
 const app = express();
+app.get("/", (req:Request, res:Response)=>{
+    res.status(200).send("OK");
+});
 
+app.use((req:Request, res:Response)=>{
+    res.status(404).send("error page")
+})
 app.listen(3000, ()=>{
     console.log('Server is running');
 });
