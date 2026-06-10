996ef0baac6b4e5140fe53b4e327332e40e040a7
Atharva <atharvakumbhar631@gmail.com>
2026-06-10 16:13:12 +0530

    octet stream

--- dist/index.js
+++ dist/index.js
@@ -1,27 +1,20 @@
 import express from "express";
-import type { Request, Response } from "express";
-
 const app = express();
-
 app.set("appName", "My express app");
-
-app.get("/", (req: Request, res: Response) => {
+app.get("/", (req, res) => {
     res.status(200).send("OK");
 });
-
-app.get("/name", (req: Request, res: Response) => {
+app.get("/name", (req, res) => {
     res.send("Application Name: " + app.get("appName"));
 });
-
-app.get("/html", (req: Request, res: Response) => {
+app.get("/html", (req, res) => {
     res.send("<h1>Hi There</h1>");
 });
-
-app.get("/buffer", (req: Request, res: Response) => {
-    const data = Buffer.from("Buffer Data");
+app.get("/buffer", (req, res) => {
+    const data = Buffer.from("Hello Buffer");
     res.send(data);
 });
-
 app.listen(3000, () => {
     console.log("Server running");
 });
+//# sourceMappingURL=index.js.map
